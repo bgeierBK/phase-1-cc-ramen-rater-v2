9c2732cd9478c2b36440e7556ba0ec1f85123180
bmgeier <geier.benjamin@gmail.com>
2024-03-19 17:03:03 -0400

    update lab

--- src/index.js
+++ src/index.js
@@ -1,21 +1,75 @@
 // index.js
 
 // Callbacks
+
 const handleClick = (ramen) => {
-  // Add code
+  document.querySelector(".detail-image").src = ramen.image;
+  document.querySelector(".name").textContent = ramen.name;
+  document.querySelector(".restaurant").textContent = ramen.restaurant;
+  document.querySelector("#rating-display").textContent = ramen.rating;
+  document.querySelector("#comment-display").textContent = ramen.comment;
 };
+
+
 
 const addSubmitListener = () => {
-  // Add code
-}
+  document.querySelector("#new-ramen").addEventListener("submit", event =>{
+    event.preventDefault();
+    fetch("http://localhost:3000/ramens", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+            "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        name: document.querySelector("#new-ramen").name.value,
+        restaurant:document.querySelector("#new-ramen").restaurant.value,
+        image:document.querySelector("#new-ramen").image.value,
+        rating:document.querySelector("#new-ramen").rating.value,
+        comment:document.querySelector("#new-ramen").comment,
+        
+      })
+    })
+    .then(response => response.json())
+ .then(newRamen=>{
+   const img= document.createElement("img");
+    img.src = newRamen.image;
+    img.setAttribute("id", newRamen.id)
+    document.querySelector("#ramen-menu").append(img);
+    
+  })
+
+  })
+  
+  }
+
+
+
+  
 
 const displayRamens = () => {
-  // Add code
-};
-
+  fetch("http://localhost:3000/ramens")
+  .then(response => response.json())
+  .then(ramens =>{
+    ramens.forEach(ramen=>{
+      const image = document.createElement("img");
+      image.src = ramen.image;
+      image.setAttribute("id", ramen.id)
+      document.querySelector("#ramen-menu").append(image);
+    })
+   handleClick(ramens[0]);
+   const image = document.getElementById("ramen-menu");
+   image.addEventListener("click", event =>{
+    ramens.forEach(ramen =>{
+      if (ramen.id == event.target.id)
+      handleClick(ramen)
+    })
+   })
+}
+  )}
 const main = () => {
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
+  document.addEventListener("DOMContentLoaded", displayRamens())
+  document.addEventListener("DOMContentLoaded", addSubmitListener())
 }
 
 main()
